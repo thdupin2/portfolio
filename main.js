3e67d53b0158a3e75471d19fac06bde26a632d5a
thdupin2 <theo.dupin03300@gmail.com>
2024-06-17 22:11:38 +0200

    :truck: rename: script.js in main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// EMAIL
+document.getElementById('openGmail').addEventListener('click', function() {
+    var email = 'theo.dupin03300.pro@gmail.com';
+    var gmailLink = 'https://mail.google.com/mail/?view=cm&fs=1&to=' + email;
+
+    // Essayer d'ouvrir le lien Gmail
+    window.open(gmailLink, '_blank');
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var aboutMeLink = document.querySelector('.header-nav a[href="#about-me"]');
+    aboutMeLink.addEventListener('click', function(event) {
+        event.preventDefault(); // Empêche le comportement par défaut du lien
+        var aboutMeSection = document.getElementById('about-me');
+        aboutMeSection.scrollIntoView({ behavior: 'smooth' }); // Fait défiler la page jusqu'à la section "À propos de moi" en douceur
+    });
+});
+
+// AOS
+AOS.init();
