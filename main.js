f8e367d56a9551892baf84b8a814299f97018638
thdupin2 <theo.dupin03300@gmail.com>
2024-06-17 22:29:42 +0200

    :hammer: update: index.html

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // EMAIL
-$(document).getElementById('openGmail').addEventListener('click', function() {
-    var email = 'theo.dupin03300.pro@gmail.com';
-    var gmailLink = 'https://mail.google.com/mail/?view=cm&fs=1&to=' + email;
+// document.getElementById('openGmail').addEventListener('click', function() {
+//     var email = 'theo.dupin03300.pro@gmail.com';
+//     var gmailLink = 'https://mail.google.com/mail/?view=cm&fs=1&to=' + email;
 
-    // Essayer d'ouvrir le lien Gmail
-    window.open(gmailLink, '_blank');
-});
+//     // Essayer d'ouvrir le lien Gmail
+//     window.open(gmailLink, '_blank');
+// });
 
-$(document).addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     var aboutMeLink = document.querySelector('.header-nav a[href="#about-me"]');
     aboutMeLink.addEventListener('click', function(event) {
         event.preventDefault(); // Empêche le comportement par défaut du lien
